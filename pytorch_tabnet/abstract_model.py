a045abe6126653f90050e9880ba742ed961442d2
kyj519 <kyj519@snu.ac.kr>
2024-01-16 22:10:16 +0900

    ~

--- pytorch_tabnet/abstract_model.py
+++ pytorch_tabnet/abstract_model.py
@@ -397,7 +397,7 @@ class TabModel(BaseEstimator):
 
         ##freezing
         for name, param in self.network.named_parameters():
-            if 'initial' in name or 'feat_transformers.0' in name or 'att_transformers.0' in name:
+            if 'initial' in name or 'feat_transformers.0' in name or 'att_transformers.0' or 'shared' in name:
                 print(f"parameter {name} will be freezed during training")
                 param.requires_grad = False
             
